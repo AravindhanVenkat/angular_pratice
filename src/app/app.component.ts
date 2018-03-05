b5a3d44a76a4886dba341f8ea8faf43123356a4c
Aravindhan <aravindhan.venkat@gmail.com>
2018-03-05 20:17:34 +0530

    Angular version modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {QuoteService} from './service/QuoteService';
+import {QuoteDetail} from './service/model/QuoteDetail';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [QuoteService]
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  public _getQuoteJsonData: QuoteDetail[];
+
+  constructor(private apiSerivce: QuoteService, private http: Http) {
+  }
+
+  private _getURL = '/lcl/quote/carrierdetails';
+  getQuoteDetail(): void {
+    this.apiSerivce.getQuoteJsonData()
+      .subscribe(resultArray => this._getQuoteJsonData = resultArray,
+      error => console.log('Error :: ' + error)
+      );
+  }
+
+  ngOnInit(): void {
+    this.getQuoteDetail();
+  }
 }
---
+++ src/app/service/model/QuoteDetail.ts
@@ -0,0 +1,5 @@
+export interface QuoteDetail {
+  status: string;
+  error: string;
+  data: string;
+}
